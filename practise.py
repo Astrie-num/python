56bdc1da63b1c7785949924ec9424cc4c7709d68
Astrie <uwasterie07@gmail.com>
2023-09-24 15:31:29 +0200

    Done the exercise about concatenation with the + operator and multiplication and replication with the * operator

---
+++ practise.py
@@ -0,0 +1,23 @@
+# print("Hello Python!!")
+# print('Hello Python!!')
+# print('''Hello Python!!''')
+# print("Astrie, A student at RCA")
+
+
+# concatenation of strings
+# name = 'astrie'
+# name2 = "vinni"
+# addition = name + age
+# addition = print(f"{name} + {age}")
+# print(addition)
+
+# replication of strings
+
+name = "George"
+age = 12
+replicate = name * age
+print(replicate)
+
+# This means that this operator can be used for both multiplication and replication
+
+
